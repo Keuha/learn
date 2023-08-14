d8eb472cf47de567ff6f565fa06722d989d5a826
Frqnck <franck.petriz@ekonoo.com>
2023-08-14 10:30:59 +0200

    Initial Commit

---
+++ Nihongo/NihongoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NihongoApp.swift
+//  Nihongo
+//
+//  Created by Franck Petriz on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NihongoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
