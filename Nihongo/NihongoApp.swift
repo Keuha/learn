db91b5213106551f342a62d95ceb8d190254dfb7
Frqnck <franck.petriz@ekonoo.com>
2023-08-20 14:53:46 +0200

    boot directly on vocabularies page

--- Nihongo/NihongoApp.swift
+++ Nihongo/NihongoApp.swift
@@ -12,7 +12,7 @@ struct NihongoApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationStack {
-                WelcomeScreen()
+                VocabularyDisplay()
             }
         }
     }
