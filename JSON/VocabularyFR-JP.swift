847263cf2dc8aaa1918947048c180185f8d4d406
Frqnck <franck.petriz@ekonoo.com>
2023-08-20 14:48:15 +0200

    uncompleted list of data for JPLT N5

---
+++ JSON/VocabularyFR-JP.swift
@@ -0,0 +1,646 @@
+
+let fr_jp = """
+[
+  {
+    "kanji": "今日",
+    "hiragana": "きょう",
+    "translation": "Aujourd'hui"
+  },
+  {
+    "kanji": "明日",
+    "hiragana": "あした",
+    "translation": "Demain"
+  },
+  {
+    "kanji": "何",
+    "hiragana": "なに",
+    "translation": "Quoi"
+  },
+  {
+    "kanji": "人",
+    "hiragana": "ひと",
+    "translation": "Personne"
+  },
+  {
+    "kanji": "名前",
+    "hiragana": "なまえ",
+    "translation": "Nom"
+  },
+  {
+    "kanji": "学校",
+    "hiragana": "がっこう",
+    "translation": "École"
+  },
+  {
+    "kanji": "先生",
+    "hiragana": "せんせい",
+    "translation": "Professeur"
+  },
+  {
+    "kanji": "学生",
+    "hiragana": "がくせい",
+    "translation": "Étudiant"
+  },
+  {
+    "kanji": "日本",
+    "hiragana": "にほん",
+    "translation": "Japon"
+  },
+  {
+    "kanji": "本",
+    "hiragana": "ほん",
+    "translation": "Livre"
+  },
+  {
+    "kanji": "田中",
+    "hiragana": "たなか",
+    "translation": "Tanaka (Nom de famille)"
+  },
+  {
+    "kanji": "一",
+    "hiragana": "いち",
+    "translation": "Un"
+  },
+  {
+    "kanji": "二",
+    "hiragana": "に",
+    "translation": "Deux"
+  },
+  {
+    "kanji": "三",
+    "hiragana": "さん",
+    "translation": "Trois"
+  },
+  {
+    "kanji": "四",
+    "hiragana": "し",
+    "translation": "Quatre"
+  },
+  {
+    "kanji": "五",
+    "hiragana": "ご",
+    "translation": "Cinq"
+  },
+  {
+    "kanji": "六",
+    "hiragana": "ろく",
+    "translation": "Six"
+  },
+  {
+    "kanji": "七",
+    "hiragana": "しち",
+    "translation": "Sept"
+  },
+  {
+    "kanji": "八",
+    "hiragana": "はち",
+    "translation": "Huit"
+  },
+  {
+    "kanji": "九",
+    "hiragana": "きゅう",
+    "translation": "Neuf"
+  },
+  {
+    "kanji": "十",
+    "hiragana": "じゅう",
+    "translation": "Dix"
+  },
+  {
+    "kanji": "お金",
+    "hiragana": "おかね",
+    "translation": "Argent"
+  },
+  {
+    "kanji": "時間",
+    "hiragana": "じかん",
+    "translation": "Temps"
+  },
+  {
+    "kanji": "今",
+    "hiragana": "いま",
+    "translation": "Maintenant"
+  },
+  {
+    "kanji": "大学",
+    "hiragana": "だいがく",
+    "translation": "Université"
+  },
+  {
+    "kanji": "魚",
+    "hiragana": "さかな",
+    "translation": "Poisson"
+  },
+  {
+    "kanji": "車",
+    "hiragana": "くるま",
+    "translation": "Voiture"
+  },
+  {
+    "kanji": "鳥",
+    "hiragana": "とり",
+    "translation": "Oiseau"
+  },
+  {
+    "kanji": "犬",
+    "hiragana": "いぬ",
+    "translation": "Chien"
+  },
+  {
+    "kanji": "猫",
+    "hiragana": "ねこ",
+    "translation": "Chat"
+  },
+  {
+    "kanji": "木",
+    "hiragana": "き",
+    "translation": "Arbre"
+  },
+  {
+    "kanji": "山",
+    "hiragana": "やま",
+    "translation": "Montagne"
+  },
+  {
+    "kanji": "川",
+    "hiragana": "かわ",
+    "translation": "Rivière"
+  },
+  {
+    "kanji": "海",
+    "hiragana": "うみ",
+    "translation": "Mer"
+  },
+  {
+    "kanji": "空",
+    "hiragana": "そら",
+    "translation": "Ciel"
+  },
+  {
+    "kanji": "花",
+    "hiragana": "はな",
+    "translation": "Fleur"
+  },
+  {
+    "kanji": "雨",
+    "hiragana": "あめ",
+    "translation": "Pluie"
+  },
+  {
+    "kanji": "雪",
+    "hiragana": "ゆき",
+    "translation": "Neige"
+  },
+  {
+    "kanji": "天気",
+    "hiragana": "てんき",
+    "translation": "Temps (météo)"
+  },
+  {
+    "kanji": "風",
+    "hiragana": "かぜ",
+    "translation": "Vent"
+  },
+  {
+    "kanji": "見る",
+    "hiragana": "みる",
+    "translation": "Regarder"
+  },
+  {
+    "kanji": "行く",
+    "hiragana": "いく",
+    "translation": "Aller"
+  },
+  {
+    "kanji": "来る",
+    "hiragana": "くる",
+    "translation": "Venir"
+  },
+  {
+    "kanji": "食べる",
+    "hiragana": "たべる",
+    "translation": "Manger"
+  },
+  {
+    "kanji": "飲む",
+    "hiragana": "のむ",
+    "translation": "Boire"
+  },
+  {
+    "kanji": "読む",
+    "hiragana": "よむ",
+    "translation": "Lire"
+  },
+  {
+    "kanji": "話す",
+    "hiragana": "はなす",
+    "translation": "Parler"
+  },
+  {
+    "kanji": "聞く",
+    "hiragana": "きく",
+    "translation": "Écouter, Entendre"
+  },
+  {
+    "kanji": "書く",
+    "hiragana": "かく",
+    "translation": "Écrire"
+  },
+  {
+    "kanji": "分かる",
+    "hiragana": "わかる",
+    "translation": "Comprendre"
+  },
+  {
+    "kanji": "飛ぶ",
+    "hiragana": "とぶ",
+    "translation": "Voler"
+  },
+  {
+    "kanji": "見える",
+    "hiragana": "みえる",
+    "translation": "Être visible"
+  },
+  {
+    "kanji": "行う",
+    "hiragana": "おこなう",
+    "translation": "Effectuer"
+  },
+  {
+    "kanji": "使う",
+    "hiragana": "つかう",
+    "translation": "Utiliser"
+  },
+  {
+    "kanji": "勉強",
+    "hiragana": "べんきょう",
+    "translation": "Étude"
+  },
+  {
+    "kanji": "終わる",
+    "hiragana": "おわる",
+    "translation": "Terminer"
+  },
+  {
+    "kanji": "始まる",
+    "hiragana": "はじまる",
+    "translation": "Commencer"
+  },
+  {
+    "kanji": "買う",
+    "hiragana": "かう",
+    "translation": "Acheter"
+  },
+  {
+    "kanji": "持つ",
+    "hiragana": "もつ",
+    "translation": "Tenir"
+  },
+  {
+    "kanji": "考える",
+    "hiragana": "かんがえる",
+    "translation": "Réfléchir"
+  },
+  {
+    "kanji": "知る",
+    "hiragana": "しる",
+    "translation": "Savoir"
+  },
+  {
+    "kanji": "教える",
+    "hiragana": "おしえる",
+    "translation": "Enseigner"
+  },
+  {
+    "kanji": "働く",
+    "hiragana": "はたらく",
+    "translation": "Travailler"
+  },
+  {
+    "kanji": "待つ",
+    "hiragana": "まつ",
+    "translation": "Attendre"
+  },
+  {
+    "kanji": "話す",
+    "hiragana": "はなす",
+    "translation": "Parler"
+  },
+  {
+    "kanji": "聞く",
+    "hiragana": "きく",
+    "translation": "Écouter, Entendre"
+  },
+  {
+    "kanji": "読む",
+    "hiragana": "よむ",
+    "translation": "Lire"
+  },
+  {
+    "kanji": "書く",
+    "hiragana": "かく",
+    "translation": "Écrire"
+  },
+  {
+    "kanji": "分かる",
+    "hiragana": "わかる",
+    "translation": "Comprendre"
+  },
+  {
+    "kanji": "食べる",
+    "hiragana": "たべる",
+    "translation": "Manger"
+  },
+  {
+    "kanji": "飲み物",
+    "hiragana": "のみもの",
+    "translation": "Boisson"
+  },
+  {
+    "kanji": "食べ物",
+    "hiragana": "たべもの",
+    "translation": "Nourriture"
+  },
+  {
+    "kanji": "朝",
+    "hiragana": "あさ",
+    "translation": "Matin"
+  },
+  {
+    "kanji": "昼",
+    "hiragana": "ひる",
+    "translation": "Midi"
+  },
+  {
+    "kanji": "晩",
+    "hiragana": "ばん",
+    "translation": "Soir"
+  },
+  {
+    "kanji": "時計",
+    "hiragana": "とけい",
+    "translation": "Montre, Horloge"
+  },
+  {
+    "kanji": "電話",
+    "hiragana": "でんわ",
+    "translation": "Téléphone"
+  },
+  {
+    "kanji": "手紙",
+    "hiragana": "てがみ",
+    "translation": "Lettre"
+  },
+  {
+    "kanji": "お土産",
+    "hiragana": "おみやげ",
+    "translation": "Souvenir"
+  },
+  {
+    "kanji": "駅",
+    "hiragana": "えき",
+    "translation": "Gare"
+  },
+  {
+    "kanji": "公園",
+    "hiragana": "こうえん",
+    "translation": "Parc"
+  },
+  {
+    "kanji": "動物",
+    "hiragana": "どうぶつ",
+    "translation": "Animal"
+  },
+  {
+    "kanji": "野菜",
+    "hiragana": "やさい",
+    "translation": "Légume"
+  },
+  {
+    "kanji": "果物",
+    "hiragana": "くだもの",
+    "translation": "Fruit"
+  },
+  {
+    "kanji": "お金持ち",
+    "hiragana": "おかねもち",
+    "translation": "Riche"
+  },
+  {
+    "kanji": "お金持ち",
+    "hiragana": "おかねもち",
+    "translation": "Riche"
+  },
+  {
+    "kanji": "一緒",
+    "hiragana": "いっしょ",
+    "translation": "Ensemble"
+  },
+  {
+    "kanji": "大好き",
+    "hiragana": "だいすき",
+    "translation": "J'adore"
+  },
+  {
+    "kanji": "嫌い",
+    "hiragana": "きらい",
+    "translation": "Détesté"
+  },
+  {
+    "kanji": "高い",
+    "hiragana": "たかい",
+    "translation": "Cher"
+  },
+  {
+    "kanji": "安い",
+    "hiragana": "やすい",
+    "translation": "Bon marché"
+  },
+  {
+    "kanji": "新しい",
+    "hiragana": "あたらしい",
+    "translation": "Nouveau"
+  },
+  {
+    "kanji": "古い",
+    "hiragana": "ふるい",
+    "translation": "Vieux"
+  },
+  {
+    "kanji": "病院",
+    "hiragana": "びょういん",
+    "translation": "Hôpital"
+  },
+  {
+    "kanji": "電車",
+    "hiragana": "でんしゃ",
+    "translation": "Train"
+  },
+  {
+    "kanji": "地下鉄",
+    "hiragana": "ちかてつ",
+    "translation": "Métro"
+  },
+  {
+    "kanji": "自動車",
+    "hiragana": "じどうしゃ",
+    "translation": "Voiture"
+  },
+  {
+    "kanji": "自転車",
+    "hiragana": "じてんしゃ",
+    "translation": "Vélo"
+  },
+  {
+    "kanji": "映画",
+    "hiragana": "えいが",
+    "translation": "Film"
+  },
+  {
+    "kanji": "音楽",
+    "hiragana": "おんがく",
+    "translation": "Musique"
+  },
+  {
+    "kanji": "歌",
+    "hiragana": "うた",
+    "translation": "Chanson"
+  },
+  {
+    "kanji": "歩く",
+    "hiragana": "あるく",
+    "translation": "Marcher"
+  },
+  {
+    "kanji": "走る",
+    "hiragana": "はしる",
+    "translation": "Courir"
+  },
+  {
+    "kanji": "泳ぐ",
+    "hiragana": "およぐ",
+    "translation": "Nager"
+  },
+  {
+    "kanji": "乗る",
+    "hiragana": "のる",
+    "translation": "Monter"
+  },
+  {
+    "kanji": "降りる",
+    "hiragana": "おりる",
+    "translation": "Descendre"
+  },
+  {
+    "kanji": "待つ",
+    "hiragana": "まつ",
+    "translation": "Attendre"
+  },
+  {
+    "kanji": "始める",
+    "hiragana": "はじめる",
+    "translation": "Commencer"
+  },
+  {
+    "kanji": "終わる",
+    "hiragana": "おわる",
+    "translation": "Terminer"
+  },
+  {
+    "kanji": "先週",
+    "hiragana": "せんしゅう",
+    "translation": "La semaine dernière"
+  },
+  {
+    "kanji": "来週",
+    "hiragana": "らいしゅう",
+    "translation": "La semaine prochaine"
+  },
+  {
+    "kanji": "毎日",
+    "hiragana": "まいにち",
+    "translation": "Tous les jours"
+  },
+  {
+    "kanji": "毎週",
+    "hiragana": "まいしゅう",
+    "translation": "Toutes les semaines"
+  },
+  {
+    "kanji": "毎月",
+    "hiragana": "まいつき",
+    "translation": "Tous les mois"
+  },
+  {
+    "kanji": "毎年",
+    "hiragana": "まいとし",
+    "translation": "Tous les ans"
+  },
+  {
+    "kanji": "時々",
+    "hiragana": "ときどき",
+    "translation": "Parfois"
+  },
+  {
+    "kanji": "今朝",
+    "hiragana": "けさ",
+    "translation": "Ce matin"
+  },
+  {
+    "kanji": "今晩",
+    "hiragana": "こんばん",
+    "translation": "Ce soir"
+  },
+  {
+    "kanji": "明日",
+    "hiragana": "あした",
+    "translation": "Demain"
+  },
+  {
+    "kanji": "昨日",
+    "hiragana": "きのう",
+    "translation": "Hier"
+  },
+  {
+    "kanji": "前",
+    "hiragana": "まえ",
+    "translation": "Avant"
+  },
+  {
+    "kanji": "後ろ",
+    "hiragana": "うしろ",
+    "translation": "Derrière"
+  },
+  {
+    "kanji": "中",
+    "hiragana": "なか",
+    "translation": "Intérieur"
+  },
+  {
+    "kanji": "外",
+    "hiragana": "そと",
+    "translation": "Extérieur"
+  },
+  {
+    "kanji": "上",
+    "hiragana": "うえ",
+    "translation": "Haut"
+  },
+  {
+    "kanji": "下",
+    "hiragana": "した",
+    "translation": "Bas"
+  },
+  {
+    "kanji": "近く",
+    "hiragana": "ちかく",
+    "translation": "Proche"
+  },
+  {
+    "kanji": "遠く",
+    "hiragana": "とおく",
+    "translation": "Loin"
+  }
+]
+""".data(using: .utf8)
+
