1a9e61d291c3ebce39ee5a6f2bff8f53a798f247
Frqnck <franck.petriz@ekonoo.com>
2023-09-12 15:05:01 +0200

    extract model to specific folder + rename `Content` to `GeneratedContent`

---
+++ Nihongo/Model/GeneratedContent.swift
@@ -0,0 +1,14 @@
+//
+//  GeneratedContent.swift
+//  Nihongo
+//
+//  Created by Franck Petriz on 12/09/2023.
+//
+
+import Foundation
+
+struct GeneratedContent: Codable, Equatable, Hashable {
+    var kanji: String = ""
+    var translation: String = ""
+    var hiragana: String = ""
+}
